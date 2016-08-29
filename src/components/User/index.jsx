30f10ddbbd6b6f000ec3c92c214e30a4bc97b329
fengmu456 <416828041@qq.com>
2016-08-29 18:48:06 +0800

    modify

--- src/components/User/index.jsx
+++ src/components/User/index.jsx
@@ -1,95 +1,95 @@
 import React from "react";
-import {Table, Card} from "antd";
+import { Table, Card } from "antd";
 import styles from "./style.less";
 import  MainLayout from "../../layouts/MainLayout";
 
 //用户头像	昵称	金币	创建时间	用户ID	用户角色	上次登录时间	设备ID	安卓金币	IDFA	操作
 let columns = [
-    {
-        title: "头像",
-        width: 60,
-        dataIndex: "Cover",
-        fixed: "left",
-        render: (text, record)=><img className={styles.head} src={text}/>
-    },
-    {title: "ID", width: 230, dataIndex: "UserId"},
-    {title: "手机号", width: 100, render: ()=><span>18883873034</span>},
-    {title: "昵称", width: 150, dataIndex: "NickName", key: "NickName"},
-    {title: "金币", width: 50, dataIndex: "Coin"},
-    {title: "Android金币", width: 100, dataIndex: "AndroidCoin"},
-    {title: "注册时间", width: 150, dataIndex: "CreatedTime"},
-    {title: "角色", width: 100, dataIndex: "Role"},
-    {title: "上次登录时间", width: 150, dataIndex: "LastLoginTime"},
-    {title: "设备ID", width: 250, dataIndex: "DeviceId"},
-    {title: "IDFA", width: 250, dataIndex: "Idfa"},
-    {title: "操作", width: 100, render: ()=><div></div>}
+	{
+		title: "头像",
+		width: 60,
+		dataIndex: "Cover",
+		//fixed: "left",
+		render: ( text, record )=><img className={styles.head} src={text}/>
+	},
+	{ title: "ID", width: 230, dataIndex: "UserId" },
+	{ title: "手机号", width: 100, render: ()=><span>18883873034</span> },
+	{ title: "昵称", width: 150, dataIndex: "NickName", key: "NickName" },
+	{ title: "金币", width: 50, dataIndex: "Coin" },
+	{ title: "Android金币", width: 100, dataIndex: "AndroidCoin" },
+	{ title: "注册时间", width: 150, dataIndex: "CreatedTime" },
+	{ title: "角色", width: 100, dataIndex: "Role" },
+	{ title: "上次登录时间", width: 150, dataIndex: "LastLoginTime" },
+	{ title: "设备ID", width: 250, dataIndex: "DeviceId" },
+	{ title: "IDFA", width: 250, dataIndex: "Idfa" },
+	{ title: "操作", width: 100, render: ()=><div></div> }
 ];
 
 let width = 0;
-columns.forEach(col=> {
-    width += col.width;
-});
+columns.forEach( col=> {
+	width += col.width;
+} );
 
 
 let data = [
-    {
-        "UserId": "18ab80a032a84cf7b6f5744f38f21b63",
-        "DeviceId": "3bf0ebe0a0a9d9da337e7e256f96f578",
-        "NickName": "手机用户_3034",
-        "Coin": 0,
-        "AndroidCoin": 159,
-        "LastLoginTime": "2016-08-26 13:29:18",
-        "CreatedTime": "2016-08-23 17:11:55",
-        "Cover": "http://img.ireadercity.com/201608252031136725.jpg",
-        "Role": "05",
-        "Idfa": "3bf0ebe0a0a9d9da337e7e256f96f578"
-    },
-    {
-        "UserId": "18ab80a032a84cf7b6f5744f38f21b63",
-        "DeviceId": "3bf0ebe0a0a9d9da337e7e256f96f578",
-        "NickName": "手机用户_3034",
-        "Coin": 0,
-        "AndroidCoin": 159,
-        "LastLoginTime": "2016-08-26 13:29:18",
-        "CreatedTime": "2016-08-23 17:11:55",
-        "Cover": "http://img.ireadercity.com/201608252031136725.jpg",
-        "Role": "05",
-        "Idfa": "3bf0ebe0a0a9d9da337e7e256f96f578"
-    },
-    {
-        "UserId": "18ab80a032a84cf7b6f5744f38f21b63",
-        "DeviceId": "3bf0ebe0a0a9d9da337e7e256f96f578",
-        "NickName": "手机用户_3035",
-        "Coin": 0,
-        "AndroidCoin": 159,
-        "LastLoginTime": "2016-08-26 13:29:18",
-        "CreatedTime": "2016-08-23 17:11:55",
-        "Cover": "http://img.ireadercity.com/201608252031136725.jpg",
-        "Role": "05",
-        "Idfa": "3bf0ebe0a0a9d9da337e7e256f96f578"
-    }
+	{
+		"UserId": "18ab80a032a84cf7b6f5744f38f21b63",
+		"DeviceId": "3bf0ebe0a0a9d9da337e7e256f96f578",
+		"NickName": "手机用户_3034",
+		"Coin": 0,
+		"AndroidCoin": 159,
+		"LastLoginTime": "2016-08-26 13:29:18",
+		"CreatedTime": "2016-08-23 17:11:55",
+		"Cover": "http://img.ireadercity.com/201608252031136725.jpg",
+		"Role": "05",
+		"Idfa": "3bf0ebe0a0a9d9da337e7e256f96f578"
+	},
+	{
+		"UserId": "18ab80a032a84cf7b6f5744f38f21b63",
+		"DeviceId": "3bf0ebe0a0a9d9da337e7e256f96f578",
+		"NickName": "手机用户_3034",
+		"Coin": 0,
+		"AndroidCoin": 159,
+		"LastLoginTime": "2016-08-26 13:29:18",
+		"CreatedTime": "2016-08-23 17:11:55",
+		"Cover": "http://img.ireadercity.com/201608252031136725.jpg",
+		"Role": "05",
+		"Idfa": "3bf0ebe0a0a9d9da337e7e256f96f578"
+	},
+	{
+		"UserId": "18ab80a032a84cf7b6f5744f38f21b63",
+		"DeviceId": "3bf0ebe0a0a9d9da337e7e256f96f578",
+		"NickName": "手机用户_3035",
+		"Coin": 0,
+		"AndroidCoin": 159,
+		"LastLoginTime": "2016-08-26 13:29:18",
+		"CreatedTime": "2016-08-23 17:11:55",
+		"Cover": "http://img.ireadercity.com/201608252031136725.jpg",
+		"Role": "05",
+		"Idfa": "3bf0ebe0a0a9d9da337e7e256f96f578"
+	}
 ];
 
 const rowSelection = {
-    onChange(selectedRowKeys, selectedRows) {
-        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-    },
-    onSelect(record, selected, selectedRows) {
-        console.log(record, selected, selectedRows);
-    },
-    onSelectAll(selected, selectedRows, changeRows) {
-        console.log(selected, selectedRows, changeRows);
-    },
+	onChange( selectedRowKeys, selectedRows ) {
+		console.log( `selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows );
+	},
+	onSelect( record, selected, selectedRows ) {
+		console.log( record, selected, selectedRows );
+	},
+	onSelectAll( selected, selectedRows, changeRows ) {
+		console.log( selected, selectedRows, changeRows );
+	},
 };
 
 export default class UserComponent extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+	constructor( props ) {
+		super( props );
+	}
 
-    render() {
-        return (
-            <Table bordered columns={columns} scroll={{x: width}} dataSource={data}/>
-        );
-    }
+	render() {
+		return (
+			<Table title={()=>"title"} bordered columns={columns} scroll={{ x: width }} dataSource={data}/>
+		);
+	}
 }
--- src/layouts/MainLayout/index.jsx
+++ src/layouts/MainLayout/index.jsx
@@ -1,111 +1,111 @@
-import React, {Component, PropTypes} from 'react';
-import {Breadcrumb, Card} from "antd";
+import React, { Component, PropTypes } from 'react';
+import { Breadcrumb, Card } from "antd";
 import TopBar from "./TopBar";
 import Menu from "./Menu";
 import styles from './style.less';
 
 let menus = [
-    {
-        title: "系统管理 ",
-        icon: "setting",
-        menus: [
-            {
-                title: "菜单管理",
-                icon: "appstore-o"
-            },
-            {
-                title: "通用配置管理",
-                icon: "setting"
-            },
-            {
-                title: "权限管理",
-                icon: "setting"
-            }
-        ]
-    },
-    {
-        title: "用户管理",
-        icon: "team",
-        menus: [
-            {
-                title: "所有用户",
-                icon: "team"
-            },
-            {
-                title: "用户异常",
-                icon: "team"
-            }
-        ]
-    },
-    {
-        title: "订单管理",
-        icon: "shopping-cart",
-        menus: [
-            {
-                title: "充值档位配置",
-                icon: "pay-circle-o"
-            },
-            {
-                title: "订单管理",
-                icon: "shopping-cart"
-            }
-        ]
-    },
-    {
-        title: "网站管理",
-        icon: "laptop",
-        menus: [
-            {
-                title: "活动管理",
-                icon: "star-o"
-            },
-            {
-                title: "书包管理",
-                icon: "folder"
-            },
-            {
-                title: "心愿书单管理",
-                icon: "folder"
-            },
-            {
-                title: "广告管理",
-                icon: "solution"
-            }
-        ]
-    }
+	{
+		title: "系统管理 ",
+		icon: "setting",
+		menus: [
+			{
+				title: "菜单管理",
+				icon: "appstore-o"
+			},
+			{
+				title: "通用配置管理",
+				icon: "setting"
+			},
+			{
+				title: "权限管理",
+				icon: "setting"
+			}
+		]
+	},
+	{
+		title: "用户管理",
+		icon: "team",
+		menus: [
+			{
+				title: "所有用户",
+				icon: "team"
+			},
+			{
+				title: "用户异常",
+				icon: "team"
+			}
+		]
+	},
+	{
+		title: "订单管理",
+		icon: "shopping-cart",
+		menus: [
+			{
+				title: "充值档位配置",
+				icon: "pay-circle-o"
+			},
+			{
+				title: "订单管理",
+				icon: "shopping-cart"
+			}
+		]
+	},
+	{
+		title: "网站管理",
+		icon: "laptop",
+		menus: [
+			{
+				title: "活动管理",
+				icon: "star-o"
+			},
+			{
+				title: "书包管理",
+				icon: "folder"
+			},
+			{
+				title: "心愿书单管理",
+				icon: "folder"
+			},
+			{
+				title: "广告管理",
+				icon: "solution"
+			}
+		]
+	}
 ];
 
-const linkRender = (href, name) => <a href={`${href}`}>{name}</a>;
+const linkRender = ( href, name ) => <a href={`${href}`}>{name}</a>;
 
 export default class MainLayout extends Component {
-    constructor(props) {
-        super(props)
-    }
+	constructor( props ) {
+		super( props )
+	}
 
-    render() {
-        return (
-            <div className={styles.continer}>
-                <TopBar/>
-                <div className={styles.content}>
-                    <div style={{display: "table", height: "100%", width: "100%"}}>
-                        <Menu menus={menus}/>
-                        <div style={{display: "table-cell", position: "relative"}}>
-                            <div style={{width: "100%", position: "absolute"}}>
-                                <div style={{padding: "20px 20px 0px 20px"}}>
-                                    <Breadcrumb  {...this.props} linkRender={linkRender}/>
-                                </div>
-                                <div style={{margin: "20px", backgroundColor: "#FFFFFF"}}>
-                                    {this.props.children}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className={styles.continer}>
+				<TopBar/>
+				<div className={styles.content}>
+					<div style={{ display: "table", height: "100%", width: "100%" }}>
+						<Menu menus={menus}/>
+						<div style={{ display: "table-cell", position: "relative" }}>
+							<div style={{ width: "100%", position: "absolute", top: "0px" }}>
+								<div style={{ padding: "20px 20px 0px 20px" }}>
+									<Breadcrumb  {...this.props} linkRender={linkRender}/>
+								</div>
+								<div style={{ margin: "20px", backgroundColor: "#FFFFFF" }}>
+									{this.props.children}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
 };
 
 MainLayout.propTypes = {
-    children: PropTypes.element.isRequired,
+	children: PropTypes.element.isRequired,
 };
